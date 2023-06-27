033dc6dec192c4465d149405657c5dbd30c44b01
Brussi <31117781+Brussi@users.noreply.github.com>
2023-06-27 17:49:00 -0300

    updates

--- explore_pcd_data.py
+++ explore_pcd_data.py
@@ -14,46 +14,72 @@ def read_files():
     return df_7days
 
 
-def get_points():
+def get_points(lower_resol_factor=1, x_limit = -34.6, mask_data = True):
     import xarray as xr
     import numpy as np
 
     fname = '../data/thresholds/limiares_2023_lower.tif'
     ds = xr.open_rasterio(fname)
-    x, y = np.meshgrid(ds.x.values, ds.y.values)
-    x = x.flatten()
-    y = y.flatten()
+    #reduce resolution
+    ds = ds.sel(x=slice(ds.x[0], -34.61))
+    data = ds[0].values
+    x = ds.x[::lower_resol_factor]
+    y = ds.y[::lower_resol_factor]
+    data = data[::lower_resol_factor, ::lower_resol_factor]
+    x, y = np.meshgrid(x, y)
 
-    mask_data = False
     if mask_data == True:
-        mask = np.where(ds[0].values.flatten()==65535)
+#        x = x.flatten()
+#        y = y.flatten()
+        #mask = np.where(ds[0].values.flatten()!=65535)
+        mask = np.where(data==65535)
+        return x, y, mask
+        # x = x[mask]
+        # y = y[mask]
 
-        x = x[mask]
-        y = y[mask]
-
-        data_dict = {"longitude": x, "latitude": y}
-        df_grid = pd.DataFrame(data_dict)
-
-    return x, y
+    return x, y, mask
 
 
-def KD(Xf, Yf, Xo, Yo, var):#f= final grid, o = observed points
+def KD(Xf, Yf, Xo, Yo, data, filter_dist=False, nn=4):#f= final grid, o = observed points
     import pyproj
     from scipy.spatial import cKDTree
 
     p = pyproj.Proj(proj='utm', zone=22, ellps='WGS84', preserve_units=True)
-
     xA, yA = p(Xf, Yf)
-    xB, yB = p(Xo, Xo)
+    xB, yB = p(Xo, Yo)
     nA = np.array(list(zip(xA, yA)) )
     nB = np.array(list(zip(xB, yB)) )
  
-    btree = cKDTree(nB)
-    dist, idx = btree.query(nA, k=4)
-    w = 1.0 / dist**2
-    air_idw = np.sum(w * var[idx], axis=1) / np.sum(w, axis=1)
+    btree = cKDTree(nB) 
+    dist, idx = btree.query(nA, k=nn)
 
-    return air_idw
+    if filter_dist == True:
+        filter = np.where(dist>100000)
+        dist[filter] = np.inf
+        #idx = idx[filter] 
+
+    w = 1.0 / dist**2
+    data_idw = np.sum(w * data[idx], axis=1) / np.sum(w, axis=1)
+    return data_idw#, filter#.reshape(shape_)
+
+
+def non_PCD_area_idx(xA, yA, xB, yB):#f= final grid, o = observed points
+    import pyproj
+    from scipy.spatial import cKDTree
+
+    shape = xA.shape
+    p = pyproj.Proj(proj='utm', zone=22, ellps='WGS84', preserve_units=True)
+    xA, yA = p(xA.flatten(), yA.flatten())
+    xB, yB = p(xB, yB)
+    nA = np.array(list(zip(xA, yA)))
+    nB = np.array(list(zip(xB, yB)))
+ 
+    btree = cKDTree(nB) 
+    dist, idx = btree.query(nA, k=1)
+    dist = dist.reshape(shape)
+    filter = np.where(dist>200000)
+        #idx = idx[filter] 
+    return filter
 
 
 def OrdinaryKriging():
@@ -72,7 +98,7 @@ def OrdinaryKriging():
     )
 
     # Fit the Kriging model
-    x, y = get_points()
+    x, y, mask = get_points()
     z, ss = OK.execute("points", x, y)
     plt.imshow(z)
     plt.show()
@@ -81,10 +107,38 @@ def OrdinaryKriging():
 def inverse_square_distance():
     df = read_files()
     rainfall_values = df.iloc[:,3:].sum(axis=1)  # Example rainfall values at each gauge
-    Xf, Yf = get_points()
+    Xf, Yf, mask_border  = get_points()
 
-    var = KD(Xf, Yf, df.longitude, df.latitude, rainfall_values.to_numpy())
+    rain_idw = KD(Xf.flatten(), Yf.flatten(), df.longitude, df.latitude, rainfall_values.to_numpy())
+    rain_idw = rain_idw.reshape(Xf.shape)
+    filter = non_PCD_area_idx(Xf, Yf, df.longitude, df.latitude)
+
+    return rain_idw
+
+def refill():
+    a=2
 
 def plot_interpolated(Xf, Yf, var):
-    plot(x, y, var)
-    plt.contourf([Xf, Yf], var)
+    # x = Xf.reshape(1561, 1806)
+    # y = Yf.reshape(1561, 1806)
+    # var = var.reshape(1561, 1806)
+    # m = Basemap(
+    #     projection = 'merc',
+    #     llcrnrlon= -74,
+    #     llcrnrlat= 5,
+    #     urcrnrlon= -29,
+    #     urcrnrlat= -34, 
+    #     resolution= 'i')
+    # m.drawcoastlines()
+    # x, y = m(x, y)
+    # cb = m.contourf(x, y, var)
+    # cbar = m.colorbar(cb, location = 'right', pad = '10%')
+    #plt.show()
+    fig = plt.figure(figsize=(10,5))
+    plt.pcolormesh(rain_idw.transpose())
+    plt.xlim([0, var.shape[0]])
+    plt.ylim([0, var.shape[1]])
+    plt.colorbar()
+    plt.title("IDW of square distance \n using 10 neighbors")
+
+
--- plot_data.py
+++ plot_data.py
@@ -1,6 +1,7 @@
 from mpl_toolkits.basemap import Basemap    
 import matplotlib.pyplot as plt
-
+import shapefile as shp
+import matplotlib.colors as mcolors
 
 def show_points(df):
     #path = image_file.parent.parent.parent
@@ -8,6 +9,8 @@ def show_points(df):
     fig.patch.set_visible(True)    
     fig.suptitle(f'PCDs - Cemaden')   
     plt.scatter(df.longitude, df.latitude, s=10,linewidths=0.2, edgecolor='black')
+    plt.scatter(x.flatten(), y.flatten(), s=1,linewidths=0.2, edgecolor='black')
+   
     #plot states
     shp = "../data/shapefiles/br_unidades_da_federacao/BRUFE250GC_SIR.shp"
     states = gpd.read_file(shp)
@@ -18,17 +21,73 @@ def show_points(df):
     plt.show()
     #fig.savefig(image_file,dpi=80,bbox_inches='tight',pad_inches=0.6) #pad_inches=-0.2, dpi=200, 
 
+def plot_map(sf, x_lim = None, y_lim = None, figsize = (11,9)):
+    plt.figure(figsize = figsize)
+    id=0
 
+#calling the function and passing required parameters to plot the full map
+plot_map(sf)
 def plot(x, y, var):
-    m = Basemap(
-        projection = 'merc',
-        llcrnrlon= -74,
-        llcrnrlat= 5,
-        urcrnrlon= -29,
-        urcrnrlat= -34, 
-        resolution= 'i')
-    m.drawcoastlines()
-    x, y = m(x, y)
-    cb = m.contourf([x,y], var)
+    # y_lim = (-35,7) # latitude
+    # x_lim = (-75, -33) # longitude
+    # shp_path = "../data/shapefiles/br_unidades_da_federacao/BRUFE250GC_SIR.shp"
+    # sf = shp.Reader(shp_path)
+    # for shape in sf.shapeRecords():
+    #     x = [i[0] for i in shape.shape.points[:]]
+    #     y = [i[1] for i in shape.shape.points[:]]w
+    #     plt.plot(x, y, 'k')
+        
+    #     if (x_lim == None) & (y_lim == None):
+    #         x0 = np.mean(x)
+    #         y0 = np.mean(y)
+    #         plt.text(x0, y0, id, fontsize=10)
+    #     id = id+1
+    
+    # if (x_lim != None) & (y_lim != None):     
+    #     plt.xlim(x_lim)
+    #     plt.ylim(y_lim)
+    fig, ax = plt.subplots(figsize=(15, 15))    
+    fig.patch.set_visible(True)    
+    fig.suptitle(f'PCDs - Cemaden')  
+
+    # draw filled contours.
+    clevs = [0, 1, 2.5, 5, 7.5, 10, 15, 20, 30, 40,
+            50, 70, 100, 150, 200, 250, 300, 400, 500, 600, 750]
+    # In future MetPy
+    # norm, cmap = ctables.registry.get_with_boundaries('precipitation', clevs)
+    cmap_data = [(1.0, 1.0, 1.0),
+                (0.3137255012989044, 0.8156862854957581, 0.8156862854957581),
+                (0.0, 1.0, 1.0),
+                (0.0, 0.8784313797950745, 0.501960813999176),
+                (0.0, 0.7529411911964417, 0.0),
+                (0.501960813999176, 0.8784313797950745, 0.0),
+                (1.0, 1.0, 0.0),
+                (1.0, 0.6274510025978088, 0.0),
+                (1.0, 0.0, 0.0),
+                (1.0, 0.125490203499794, 0.501960813999176),
+                (0.9411764740943909, 0.250980406999588, 1.0),
+                (0.501960813999176, 0.125490203499794, 1.0),
+                (0.250980406999588, 0.250980406999588, 1.0),
+                (0.125490203499794, 0.125490203499794, 0.501960813999176),
+                (0.125490203499794, 0.125490203499794, 0.125490203499794),
+                (0.501960813999176, 0.501960813999176, 0.501960813999176),
+                (0.8784313797950745, 0.8784313797950745, 0.8784313797950745),
+                (0.9333333373069763, 0.8313725590705872, 0.7372549176216125),
+                (0.8549019694328308, 0.6509804129600525, 0.47058823704719543),
+                (0.6274510025978088, 0.42352941632270813, 0.23529411852359772),
+                (0.4000000059604645, 0.20000000298023224, 0.0)]
+    cmap = mcolors.ListedColormap(cmap_data, 'precipitation')
+    norm = mcolors.BoundaryNorm(clevs, cmap.N)
+
+    #cb = plt.tricontourf(Xf.flatten(),Yf.flatten(), rain_idw.flatten())
+    rain_idw[mask_border] = np.nan
+    rain_idw[filter] = np.nan
+    cb = plt.contourf(Xf,Yf, rain_idw.reshape(Xf.shape),clevs,cmap=cmap, norm=norm)
+
+    #plt.imshow(rain_idw)
     cbar = m.colorbar(cb, location = 'right', pad = '10%')
+    shp = "../data/shapefiles/br_unidades_da_federacao/BRUFE250GC_SIR.shp"
+    states = gpd.read_file(shp)
+    states.boundary.plot(ax=ax, linewidth=1, edgecolor='black')
+
     plt.show()
